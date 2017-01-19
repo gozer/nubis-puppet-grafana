4dc53a179ad366406374f304bbcc24d437badfdd
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-01-19 11:50:50 -0500

    import grafana stuff

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,7 +42,81 @@
 #
 # Copyright 2017 Your name here, unless otherwise noted.
 #
-class nubis_grafana {
+class nubis_grafana($version = '3.1.1-1470047149', $tag_name='monitoring', $project=undef) {
+  if (!$project) {
+    $project = $::project_name
+  }
+  
+  package {'crudini':
+    ensure => present,
+  }
+
+class { 'grafana':
+  install_method => 'repo',
+  version        => $version,
+  cfg            => {
+    app_mode          => 'production',
+    'server'          => {
+      protocol => 'http',
+      root_url => '/grafana',
+    },
+    'auth.anonymous'  => {
+      enabled => true,
+    },
+    # Needs to be disabled for traefik, enabled for grafana_datasource, hurgh
+    'auth.basic'      => {
+      enabled => true,
+    },
+    users             => {
+      allow_sign_up => false,
+    },
+    'dashboards.json' => {
+      enabled => true,
+    },
+  },
+}->
+exec {'wait-for grafana startup':
+  command => '/bin/sleep 15',
+}->
+grafana_datasource { 'prometheus':
+  grafana_url      => 'http://localhost:3000',
+  grafana_user     => 'admin',
+  grafana_password => 'admin',
+  type             => 'prometheus',
+  url              => 'http://localhost:81/prometheus',
+  access_mode      => 'proxy',
+  is_default       => true,
+}->
+exec { 'disable basic auth':
+  command => '/usr/bin/crudini --set /etc/grafana/grafana.ini auth.basic enabled false',
+  require => [
+    Package['crudini'],
+  ]
+}->
+exec {'enable proxy support':
+  command => '/bin/echo ". /etc/profile.d/proxy.sh" >> /etc/default/grafana-server'
+}
+
+  file { '/var/lib/grafana/dashboards':
+  ensure  => directory,
+  owner   => grafana,
+  group   => grafana,
+  mode    => '0640',
+  recurse => true,
+  purge   => true,
+  source  => 'puppet:///modules/${module_name}/dashboards',
+  require => [
+    Class['grafana'],
+  ]
+  }
+
+  file { '/etc/consul/svc-grafana.json':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template("${module_name}/svc-grafana.json.tmpl"),
+  }
 
 
 }
